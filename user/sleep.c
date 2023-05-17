682a5905fc1554727fbe79993f50d01c0725e52d
“yxf” <919642307@qq.com>
2023-05-17 14:25:40 +0800

    test sleep

---
+++ user/sleep.c
@@ -0,0 +1,14 @@
+//
+// Created by 91964 on 2023/5/16.
+//
+
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+
+int main(int argc, char *argv[]){
+    int num = atoi(argv[1]);
+    cout << num;
+
+}
